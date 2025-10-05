a704b0bfb0a8e2edde72acaba712266d402721f6
Swarnabha Banerjee <swarnabha.banerjee2005@gmail.com>
2025-10-05 23:44:32 +0530

    Calculate and plot mean monthly maximum temperature

--- code/abu_max_temp.m
+++ code/abu_max_temp.m
@@ -1,25 +1,23 @@
 data = readtable('climate_change_upto_2000_1.csv');
 
-
 abuRows = strcmp(data.StationName, 'Abu');
-
 abuData = data(abuRows, :);
-
 months = {'January','February','March','April','May','June','July','August','September','October','November','December'};
 
-abuMinTemp = zeros(12, 1);
+abuMaxTemp = zeros(12, 1);
 
 for i = 1:12
     % Find row for current month
     idx = strcmp(abuData.Month, months{i});
     % Store the max temp for that month
-    abuMinTemp(i) = abuData.MeanTemperatureInDegreeC_Minimum(idx);
+    abuMinTemp(i) = abuData.MeanTemperatureInDegreeC_Maximum(idx);
 end
 
 figure;
-plot(1:12, abuMinTemp, '-o');
+plot(1:12, abuMaxTemp, '-o');
 set(gca, 'XTick', 1:12, 'XTickLabel', months);
 xlabel('Month');
-ylabel('Mean Min Temperature (°C)');
-title('Abu: Mean Monthly Minimum Temperature (1901-2000)');
+ylabel('Mean Max Temperature (°C)');
+title('Abu: Mean Monthly Maximum Temperature (1901-2000)');
 grid on;
+
